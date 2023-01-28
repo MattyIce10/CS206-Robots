bbd8489c389ac4d2d3e2a2ce22367e5d07b16246
Matthew Thomas <mthoma23@uvm.edu>
2023-01-27 21:44:28 -0500

    finished module D

--- generate.py
+++ generate.py
@@ -1,4 +1,23 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+length = 1; width = 1; height = 1
+x = 0; y = 0; z = .5
+cubesx = 0
+cubesy = 0
+while cubesy < 5:
+    y += 1
+    while cubesx < 5:
+        x += 1
+        for c in range(9):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+            z+=1
+            length = length * .9
+            width = width * .9
+            height = height * .9
+        length = 1; width = 1; height = 1
+        cubesx += 1
+    cubesy += 1
+    cubesx = 0
+    x = 0
+
 pyrosim.End()
