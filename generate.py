f67fb6b0b008a924289402e4ecc41b21cf799b69
Matthew Thomas <mthoma23@uvm.edu>
2023-02-06 15:32:28 -0500

    start of sensor hw

--- generate.py
+++ generate.py
@@ -1,23 +1,26 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
-length = 1; width = 1; height = 1
-x = 0; y = 0; z = .5
-cubesx = 0
-cubesy = 0
-while cubesy < 5:
-    y += 1
-    while cubesx < 5:
-        x += 1
-        for c in range(9):
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-            z+=1
-            length = length * .9
-            width = width * .9
-            height = height * .9
-        length = 1; width = 1; height = 1
-        cubesx += 1
-    cubesy += 1
-    cubesx = 0
-    x = 0
 
-pyrosim.End()
+length = 1; width = 1; height = 1
+
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    x = -2; y = 2; z = .5
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])           
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    x = 1.5; y = 0; z = 3.5
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,3])
+    x = -.5; y = 0; z = -.5
+    pyrosim.Send_Cube(name="BackLeg", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1,0,0])
+    x = .5; y = 0; z = -.5
+    pyrosim.Send_Cube(name="FrontLeg", pos=[x,y,z] , size=[length,width,height])
+   
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
